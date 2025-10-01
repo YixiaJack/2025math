4f023ea3d907234ec17f9276cbbace27aa78954e
YixiaJack <yy5091@nyu.edu>
2025-10-01 01:20:40 -0400

    22

--- Assignment/UA228/import numpy as np.py
+++
@@ -1,8 +0,0 @@
-
-col=[[] for _ in range(10)]
-with open(filename,'r') as f:
-    for lines in f:
-        numbers=lines.strip().split()
-        for i in range(10):
-            col[i].append(int(numbers[i]))
-for co
